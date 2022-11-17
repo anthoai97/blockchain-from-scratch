1a5cce4daa600606be9de8d24f8d68c50e83ee46
An Thoại <anquach.dev@gmail.com>
2022-11-17 21:11:09 +0700

    start a virtal machine

---
+++ core/vm_test.go
@@ -0,0 +1,22 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVM(t *testing.T) {
+	// 1 + 2 = 3
+	// 1
+	// push stack
+	// 2
+	// add
+	// 3
+	// push stack
+	data := []byte{0x02, 0x0a, 0x02, 0x0a, 0x0b}
+	vm := NewVM(data)
+
+	assert.Nil(t, vm.Run())
+	assert.Equal(t, byte(4), vm.stack[vm.sp])
+}
--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"log"
-	"math/rand"
-	"strconv"
 	"time"
 
 	"github.com/anthoai97/blockchain-from-scratch/core"
@@ -84,7 +82,7 @@ func initRemoteServers(trs []network.Transport) {
 
 func sendTranasction(tr network.Transport, to network.NetAddr) error {
 	privKey := crypto.GeneratePrivateKey()
-	data := []byte(strconv.FormatInt(int64(rand.Intn(1000000000)), 10))
+	data := []byte{0x02, 0x0a, 0x02, 0x0a, 0x0b}
 	tx := core.NewTransaction(data)
 	tx.Sign(privKey)
 	buf := &bytes.Buffer{}
