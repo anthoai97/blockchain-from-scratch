6246ed0af834cb89b5a0988d547118a41f2085e7
An Thoại <anquach.dev@gmail.com>
2022-11-17 21:53:33 +0700

    vm support string

--- core/vm_test.go
+++ core/vm_test.go
@@ -1,10 +1,23 @@
 package core
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestStack(t *testing.T) {
+	s := NewStack(128)
+	s.Push(1)
+	s.Push(2)
+
+	value := s.Pop()
+
+	assert.Equal(t, value, 1)
+
+	fmt.Println(s.data)
+}
 
 func TestVM(t *testing.T) {
 	// 1 + 2 = 3
@@ -14,9 +27,11 @@ func TestVM(t *testing.T) {
 	// add
 	// 3
 	// push stack
-	data := []byte{0x02, 0x0a, 0x02, 0x0a, 0x0b}
+	// data := []byte{0x03, 0x0a, 0x02, 0x0a, 0x0e}
+	data := []byte{0x03, 0x0a, 0x46, 0x0c, 0x4f, 0x0c, 0x4f, 0x0c, 0x0d}
 	vm := NewVM(data)
-
 	assert.Nil(t, vm.Run())
-	assert.Equal(t, byte(4), vm.stack[vm.sp])
+	result := vm.stack.Pop().([]byte)
+
+	assert.Equal(t, "FOO", string(result))
 }
