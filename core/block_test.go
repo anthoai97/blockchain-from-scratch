9bb8ef9df94ca2465cb9cfcae64050e881f963fe
An Quach <anquach.dev@gmail.com>
2022-11-17 10:53:44 +0700

    add encode/decode block

--- core/block_test.go
+++ core/block_test.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
 	"time"
@@ -34,6 +35,16 @@ func TestVerifyBlock(t *testing.T) {
 
 	b.Height = 100
 	assert.NotNil(t, b.Verify())
+}
+
+func TestDecodeEncodeBlock(t *testing.T) {
+	b := randomBlock(t, 1, types.Hash{})
+	buf := &bytes.Buffer{}
+	assert.Nil(t, b.Encode(NewGobBlockEncoder(buf)))
+
+	bDecode := new(Block)
+	assert.Nil(t, bDecode.Decode(NewGobBlockDecoder(buf)))
+	assert.Equal(t, b, bDecode)
 }
 
 func randomBlock(t *testing.T, height uint32, previousBlockHash types.Hash) *Block {
